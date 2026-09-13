a541490fe42c891889180bbade499c30def5aed8
Sergey Balakhonov <sergey.balakhonov@yandex.ru>
2026-09-14 00:47:30 +0300

    Начало разработки.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@
  */
 function calculateSimpleRevenue(purchase, _product) {
    // @TODO: Расчет выручки от операции
+   const { discount, sale_price, quantity } = purchase;
 }
 
 /**
@@ -17,6 +18,7 @@ function calculateSimpleRevenue(purchase, _product) {
  */
 function calculateBonusByProfit(index, total, seller) {
     // @TODO: Расчет бонуса от позиции в рейтинге
+    const { profit } = seller;
 }
 
 /**
@@ -27,13 +29,31 @@ function calculateBonusByProfit(index, total, seller) {
  */
 function analyzeSalesData(data, options) {
     // @TODO: Проверка входных данных
-
+    if (!data || !Array.isArray(data.sellers) || data.sellers.length === 0) {
+        throw new Error ('Некорректные входные данные');
+    }
     // @TODO: Проверка наличия опций
-
+    const { calculateRevenue, calculateBonus } = options;
+    // if (calculateRevenue !== "function" || calculateBonus !== "function") {
+    //     throw new Error ('Переданные параметры не являются функциями');
+    // }
     // @TODO: Подготовка промежуточных данных для сбора статистики
-
+    const sellerStats = data.sellers.map(seller => ({
+        id: seller.id,
+        name: `${seller.first_name} ${seller.last_name}`,
+        revenue: 0,
+        profit: 0,
+        sales_count: 0,
+        products_sold: {}
+        }));
+    console.log(sellerStats);
     // @TODO: Индексация продавцов и товаров для быстрого доступа
-
+    const sellerIndex = data.sellers.reduce((index, seller) => {
+        index[seller.id] = seller;
+        return index;
+    }, {});
+    console.log(sellerIndex);
+    const productIndex = {};
     // @TODO: Расчет выручки и прибыли для каждого продавца
 
     // @TODO: Сортировка продавцов по прибыли
